e63fa849b2210cd75eab9bafaf9d5c6a18d4584a
Sorekjen <sorenkjellerup@gmail.com>
2023-05-07 14:25:42 +0200

    renamed folder + simplified tests renamed the folder test to tests simplified the tests for the purpose of figuring out how to run them in docker

---
+++ src/tests/nav.test.js
@@ -0,0 +1,48 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import Hjem from "../routes/Hjem.js";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+
+it("renders without crashing", () => {
+    render(<Hjem />);
+
+  });
+
+/*import Hjem from "../routes/Hjem.js";
+import "@testing-library/jest-dom";
+import Information from "../routes/Information.js";
+
+it("Hjem render forsidetekst", () => {
+    render(<Hjem />)
+    const element = screen.getByText("Forside!")
+    console.log(element)
+    expect(element).toBeInTheDocument()
+})
+
+it("Temperaturbutton skifter til temperatur", () => {
+    const container = render(<Information />).container
+    const button = container.querySelector("#temperatureButton");
+    expect(screen.getByText("TemperatureData")).not.toBeVisible()
+    fireEvent.click(button)
+    expect(screen.getByText("TemperatureData")).toBeVisible()
+})
+
+it("Humiditybutton skifter til humidity", () => {
+    const container = render(<Information />).container
+    const button = container.querySelector("#humidityButton");
+    expect(screen.getByText("HumidityData")).not.toBeVisible()
+    fireEvent.click(button)
+    expect(screen.getByText("HumidityData")).toBeVisible()
+})
+
+it("co2button skifter til co2", () => {
+    const container = render(<Information />).container
+    const button = container.querySelector("#co2Button");
+    expect(screen.getByText("CO2Data")).not.toBeVisible()
+    fireEvent.click(button)
+    expect(screen.getByText("CO2Data")).toBeVisible()
+})*/
+
