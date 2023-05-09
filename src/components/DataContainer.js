fdeb04ab9f059b7a2bed431319711d958e69aab1
Sorekjen <sorenkjellerup@gmail.com>
2023-05-09 21:31:34 +0200

    test: made test for DataContainer Added ids to DataContainer for testing purposes. Made DataContainer.test.js and added test for DataContainer.

--- src/components/DataContainer.js
+++ src/components/DataContainer.js
@@ -1,22 +1,21 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import TimeSelect from "./TimeSelect";
 import DataTable from "./DataTable";
 
-function DataContainer({data, dataName, setEndDate})
-{
+function DataContainer({ data, dataName, setEndDate }) {
     const [displayType, setDisplayType] = useState("graf");
     //Hej fra Pernille og CO
 
     return (
         <>
-            <div className="displayContainer">
+            <div data-testid={`${dataName}-container`} className="displayContainer">
                 <div className="displayToggle" >
-                <button className={`databutton ` + (displayType === "graf" ? "highlighted" : "")} onClick={()=> setDisplayType("graf")}>Graf</button>
-                <button className={`databutton ` + (displayType === "table" ? "highlighted" : "")} onClick={()=> setDisplayType("table")}>Table</button>
+                    <button id={`grafbutton`} className={`databutton ` + (displayType === "graf" ? "highlighted" : "")} onClick={() => setDisplayType("graf")}>Graf</button>
+                    <button id={`tablebutton`} className={`databutton ` + (displayType === "table" ? "highlighted" : "")} onClick={() => setDisplayType("table")}>Table</button>
                 </div>
-                <TimeSelect setEndDate={setEndDate}/>
-                <DataTable data={data} dataName={dataName}/>
+                <TimeSelect setEndDate={setEndDate} />
+                <DataTable data={data} dataName={dataName} />
             </div>
         </>
     )
-}  export default DataContainer
+} export default DataContainer
